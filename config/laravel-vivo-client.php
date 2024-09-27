c87b197a460759392fa4afbf04670ca5f3bb34b9
David Panama <desarrollo.escire@outlook.com>
2024-09-27 16:29:24 -0600

    change config var

--- config/laravel-vivo-client.php
+++ config/laravel-vivo-client.php
@@ -5,6 +5,7 @@ return [
     'url_vivo' => env('LVC_URL_VIVO'),
     'email_user_vivo' => env('LVC_EMAIL_USER_VIVO'),
     'password_user_vivo' => env('LVC_PASSWORD_USER_VIVO'),
-    'graph_vivo' => env('LVC_GRAPH_VIVO'),
+    'graph_main_vivo' => env('LVC_GRAPH_MAIN_VIVO'),
     'individual_url_vivo' => env('LVC_INDIVIDUAL_URL_VIVO'),
+    'individual_type_vivo' => env('LVC_INDIVIDUAL_TYPE_VIVO'),
 ];
