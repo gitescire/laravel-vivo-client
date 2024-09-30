c5b37880a928bd7aa613440e9c22d89bfc5de558
David Panama <desarrollo.escire@outlook.com>
2024-09-30 11:05:35 -0600

    guzzle update

--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -8,6 +8,7 @@ class ComposerStaticInite71329279ae9e877dbdb1ade9b15953b
 {
     public static $files = array (
         '7b11c4dc42b3b3023073cb14e519683c' => __DIR__ . '/..' . '/ralouphie/getallheaders/src/getallheaders.php',
+        'c964ee0ededf28c96ebd9db5099ef910' => __DIR__ . '/..' . '/guzzlehttp/promises/src/functions_include.php',
         '6e3fae29631ef280660b3cdad06f25a8' => __DIR__ . '/..' . '/symfony/deprecation-contracts/function.php',
         '37a3dc5111fe8f707ab4c132ef1dbc62' => __DIR__ . '/..' . '/guzzlehttp/guzzle/src/functions_include.php',
     );
@@ -23,10 +24,7 @@ class ComposerStaticInite71329279ae9e877dbdb1ade9b15953b
             'GuzzleHttp\\Psr7\\' => 16,
             'GuzzleHttp\\Promise\\' => 19,
             'GuzzleHttp\\' => 11,
-        ),
-        'E' => 
-        array (
-            'Escire\\LaravelVivoClient\\' => 25,
+            'Gitescire\\LaravelVivoClient\\' => 28,
         ),
     );
 
@@ -52,7 +50,7 @@ class ComposerStaticInite71329279ae9e877dbdb1ade9b15953b
         array (
             0 => __DIR__ . '/..' . '/guzzlehttp/guzzle/src',
         ),
-        'Escire\\LaravelVivoClient\\' => 
+        'Gitescire\\LaravelVivoClient\\' => 
         array (
             0 => __DIR__ . '/../..' . '/src',
         ),
